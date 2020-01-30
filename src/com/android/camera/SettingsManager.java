8e1ed035943d00d5aba4c28440c36b9f460f775c
Jan Altensen <info@stricted.net>
2020-01-31 00:24:47 +0100

    Snap: catch NumberFormatException in getDeviceSocId
     * non qcom devices might return a non numeric value
    
    Change-Id: I6dcdab18965778053bf10291c9fa86e21d048c4f

--- src/com/android/camera/SettingsManager.java
+++ src/com/android/camera/SettingsManager.java
@@ -89,6 +89,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.lang.NumberFormatException;
 import java.lang.StringBuilder;
 
 public class SettingsManager implements ListMenu.SettingsListener {
@@ -885,7 +886,7 @@ public class SettingsManager implements ListMenu.SettingsListener {
                     }
                 } catch (FileNotFoundException e) {
                     Log.d(TAG, filePath + " doesn't found!");
-                } catch (IOException e) {
+                } catch (IOException | NumberFormatException e) {
                     Log.d(TAG, filePath + " read exception, " + e.getMessage());
                 }
             }
